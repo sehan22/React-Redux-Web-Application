671bdf1867074abeb746e51c24308af19191622a
sehan22 <sehanranaweera2002@gmail.com>
2024-02-16 23:12:35 +0530

    Rendered the fetched data on the front-end.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import ButtonComponent from "./components/ButtonComponent";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="flex flex-col gap-3 text-white justify-center items-center w-screen h-screen bg-gray-600">
         Click here to continue..
         <ButtonComponent/>
+        <br/>
+        <PostList/>
     </div>
   );
 }
