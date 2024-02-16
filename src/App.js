5073ec8a16b31dafdfb06080f6402a792b68bf53
sehan22 <sehanranaweera2002@gmail.com>
2024-02-16 19:30:54 +0530

    second commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonComponent from "./components/ButtonComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-3 text-white justify-center items-center w-screen h-screen bg-gray-600">
+        Click here to continue..
+        <ButtonComponent/>
     </div>
   );
 }
---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ButtonComponent(props) {
+    return (
+        <button type="button" className="px-4 py-2 rounded-lg bg-cyan-600 text-white">Click me</button>
+    );
+}
+
+export default ButtonComponent;
